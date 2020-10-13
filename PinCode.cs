592ea261b132d865f22633cf57f62c5727bf5552
Aryav <tiwarikaustav@gmail.com>
2020-10-13 13:51:28 +0530

    [Aryav] any string ending with six consecutive number matches

---
+++ PinCode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PinCodeEmailValidator
+{
+    class PinCode
+    {
+        string pinCodeNum;
+
+        public void EnterPinCode()
+        {
+            Console.WriteLine("Please enter Pin Code:");
+            string pinCodeNum = Console.ReadLine();
+            this.pinCodeNum = pinCodeNum;
+        }
+        public void IsValidatePinCode()
+        {
+            string pinCodeRegex = @"[0-9]{6}$";
+            if (Regex.IsMatch(pinCodeNum, pinCodeRegex))
+            {
+                Console.WriteLine("Pin Code is Validated. You are good to go!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pin Code!");
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PinCodeEmailValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PinCode pinCode = new PinCode();
+            pinCode.EnterPinCode();
+            pinCode.IsValidatePinCode();
+        }
+    }
+}
